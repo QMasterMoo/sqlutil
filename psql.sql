db762bc54bad59cf3cd2ed7bf146c7e4d863f55b
Alex Harding <alex.harding@whale-net.net>
2022-09-18 20:47:22 -0400

    Update psql.sql

--- psql.sql
+++ psql.sql
@@ -1,15 +1,18 @@
 -- common psql scripts I want to save
 
+-- generate indexes
 select t.schemaname, t.tablename
     , a.attname as columnname
+    , concat('create index ix_', t.tablename, '_', a.attname, ' ON ', t.tablename, ' ("',a.attname,'");')
 from pg_tables t
-inner join pg_class tc ON t.tablename = tc.relname -- todo - better join neede
+inner join pg_class tc ON t.tablename = tc.relname
 inner join pg_attribute a on tc.oid = a.attrelid
-where tc.relnamespace = --schema oid
+where tc.relnamespace = 16386--f1stats
 and not exists -- all columns not in an index
     (SELECT *
     from pg_index i2
     inner join pg_attribute a2 ON i2.indrelid = tc.oid
+        --and unnest(i2.indkey) = a.attnum
         and a2.attnum = any(i2.indkey)
     where i2.indrelid = tc.oid -- tieback to table
     and a2.attnum = a.attnum
@@ -25,3 +28,5 @@ and exists -- column name exists in primary key of another table
     and i2.indrelid <> tc.oid
     and ia2.attname = a.attname -- name not num
     )
+    
+-- todo generate foreign keys where not exists
